27471732a3453a3c309f6d9132ba1fa7c87d98a7
OriolITIC <2023_oriol.martinez@iticbcn.cat>
2024-05-27 20:39:01 +0200

    Añadiendo repositorios categoria y subcategorya y clases servicios vacias

--- src/main/java/com/accesadades/botiga/Model/Category.java
+++ src/main/java/com/accesadades/botiga/Model/Category.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Category")
+@Table(name = "category")
 public class Category implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/accesadades/botiga/Service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.accesadades.botiga.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+import com.accesadades.botiga.Model.Product;
+import com.accesadades.botiga.Repository.ProductRepository;
+
+@Service
+public class CategoryService {
+    
+}
