9c35b35821b4764c2f5d3c8092f9850653c0b384
JinhaoChenDAM <Jchen22@jaumebalmes.net>
2024-05-28 23:46:22 +0200

    Modificar endpoint /form y /products/desar

--- src/main/java/com/accesadades/botiga/Service/SubcategoryService.java
+++ src/main/java/com/accesadades/botiga/Service/SubcategoryService.java
@@ -1,13 +1,9 @@
 package com.accesadades.botiga.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import java.util.Set;
-import com.accesadades.botiga.Model.Product;
 import com.accesadades.botiga.Model.Subcategory;
-import com.accesadades.botiga.Repository.ProductRepository;
 
-@Service
+
 public interface SubcategoryService {
     Set<Subcategory> findAllSubcategories();
     Subcategory findSubcategoryByName(String name);
